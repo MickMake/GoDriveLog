b56b62f0c4fa3b1c9f74eab9ce42e3666b0c6079
MickMake <github@mickmake.com>
2026-06-06 18:58:53 +1000

    retro1: add palette

---
+++ widgets/retro1/palette.go
@@ -0,0 +1,19 @@
+package retro1
+
+import "image/color"
+
+var (
+	panelBG     = color.NRGBA{R: 3, G: 5, B: 6, A: 255}
+	panelFrame  = color.NRGBA{R: 18, G: 26, B: 28, A: 255}
+	panelInset  = color.NRGBA{R: 0, G: 9, B: 7, A: 255}
+	gridDim     = color.NRGBA{R: 30, G: 64, B: 48, A: 120}
+	greenOn     = color.NRGBA{R: 80, G: 255, B: 72, A: 255}
+	amberOn     = color.NRGBA{R: 255, G: 186, B: 38, A: 255}
+	redOn       = color.NRGBA{R: 255, G: 45, B: 38, A: 255}
+	ghostGreen  = color.NRGBA{R: 80, G: 255, B: 72, A: 45}
+	ghostAmber  = color.NRGBA{R: 255, G: 186, B: 38, A: 45}
+	ghostRed    = color.NRGBA{R: 255, G: 45, B: 38, A: 45}
+	textAmber   = color.NRGBA{R: 255, G: 204, B: 80, A: 255}
+	labelGreen  = color.NRGBA{R: 134, G: 255, B: 142, A: 230}
+	needleWhite = color.NRGBA{R: 230, G: 255, B: 235, A: 255}
+)
