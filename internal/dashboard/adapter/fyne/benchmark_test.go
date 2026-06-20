407ca499fd7f3d769c06d9181223543a0f7f6ac4
MickMake <github@mickmake.com>
2026-06-20 23:47:05 +1000

    test(v3.2): split seven-segment adapter benchmarks

--- internal/dashboard/adapter/fyne/benchmark_test.go
+++ internal/dashboard/adapter/fyne/benchmark_test.go
@@ -5,10 +5,22 @@ import (
 	"path/filepath"
 	"testing"
 
+	fynetest "fyne.io/fyne/v2/test"
+
 	"github.com/MickMake/GoDriveLog/internal/dashboard/v3dashboard"
 )
 
-func BenchmarkSevenSegmentAdapterUpdate(b *testing.B) {
+func BenchmarkSevenSegmentAdapterUpdateNoRefresh(b *testing.B) {
+	benchmarkSevenSegmentAdapterUpdate(b, false)
+}
+
+func BenchmarkSevenSegmentAdapterUpdateWithFyneRefresh(b *testing.B) {
+	app := fynetest.NewApp()
+	defer app.Quit()
+	benchmarkSevenSegmentAdapterUpdate(b, true)
+}
+
+func benchmarkSevenSegmentAdapterUpdate(b *testing.B, withRefresh bool) {
 	dir := b.TempDir()
 	for _, asset := range []string{"panel.png", "glass.png"} {
 		if err := writeTestPNG(filepath.Join(dir, "assets", asset)); err != nil {
@@ -23,6 +35,9 @@ func BenchmarkSevenSegmentAdapterUpdate(b *testing.B) {
 	adapter, err := New(dir)
 	if err != nil {
 		b.Fatal(err)
+	}
+	if !withRefresh {
+		disableRefresh(adapter)
 	}
 
 	b.ReportAllocs()
